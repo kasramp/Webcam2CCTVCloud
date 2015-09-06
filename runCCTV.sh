fb480b5b07b6a11a4346b7ae5ce30e80d93e971a
kasramp <kasra_mp@yahoo.com>
2015-09-05 23:24:44 -0500

    adding GNU GPL license

--- runCCTV.sh
+++ runCCTV.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# This file is part of Webcam2CCTVCloud.
+# Webcam2CCTVCloud is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3
+# as published by the Free Software Foundation.
+#
+# Webcam2CCTVCloud is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.  <http://www.gnu.org/licenses/>
+#
+# Author(s):
+# © 2015 Kasra Madadipouya <kasra@madadipouya.com>
+
 DELAY="-s"
 LOCATION="-p"
 SECOND=10
